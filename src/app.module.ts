129c9a18740f2c8c981929f80416c5540f508d7c
devstefancho <skku7714@gmail.com>
2023-12-30 17:17:34 +0900

    add markdown api with mock data

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MarkdownController } from './markdown/markdown.controller';
+import { MarkdownService } from './markdown/markdown.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, MarkdownController],
+  providers: [AppService, MarkdownService],
 })
 export class AppModule {}
---
+++ src/markdown/markdown.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@nestjs/common';
+import * as fs from 'fs';
+import * as path from 'path';
+import { marked } from 'marked';
+import matter from 'gray-matter';
+
+@Injectable()
+export class MarkdownService {
+  async parseMarkdownFile(
+    filePath: string,
+  ): Promise<{ html: string; frontmatter: any }> {
+    const fullPath = path.join(
+      process.cwd(),
+      'data',
+      'content',
+      `${filePath}.md`,
+    );
+    const fileContents = fs.readFileSync(fullPath, 'utf8');
+    const parsedMarkdown = matter(fileContents);
+
+    return {
+      html: marked(parsedMarkdown.content) as string,
+      frontmatter: parsedMarkdown.data,
+    };
+  }
+}
