ee7642d3b1dedf09277fc10b31e56f37acb84041
devstefancho <skku7714@gmail.com>
2023-12-30 18:21:25 +0900

    checkout open-wiki specific commit

--- prebuild.sh
+++ prebuild.sh
@@ -3,6 +3,9 @@
 # 임시 디렉토리에 저장소를 클론합니다.
 git clone https://github.com/devstefancho/open-wiki.git tmp_open-wiki
 
+# 저장소의 특정 커밋으로 체크아웃합니다.
+git checkout c49699e3ef53160ba779b733016940ab87352db4
+
 mkdir -p data/blog
 
 # 필요한 파일 또는 디렉토리를 data 디렉토리로 복사합니다.
